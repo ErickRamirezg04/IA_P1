5c636637bf27e713bb5d7ff45f7c65c25f438539
ErickRG2004 <erick.ramirezg04@gmail.com>
2026-02-24 18:39:36 -0600

    Practica 3

---
+++ E1-Operadores.py
@@ -0,0 +1,12 @@
+#Operadores: permiten realizar distintas operaciones con los numeros
+#Si almenos uno de los terminos es flotante, el resultado sera flotante
+#Suma +
+#Resta -
+#Multiplicación *
+#Division /
+#** Exponencial
+print(2 ** 3)
+#Division entera: resultado redondeado siempre hacia el entero inferior
+print(6 // 3)
+#Residuo(modulo): Resultado que queda de la division entera
+print(14 % 4)
