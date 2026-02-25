12f55b0ae6fc92551d792c88eb345fc9b028b0c4
ErickRG2004 <erick.ramirezg04@gmail.com>
2026-02-24 18:36:07 -0600

    Practica 2

---
+++ E1-Literales.py
@@ -0,0 +1,24 @@
+#Esta sigue siendo una cadena
+print("2")
+#Esto es un literal
+print(2)
+#Se deben escribir seguidos o con _
+print(10349)
+print(10_349)
+#Puedes usar sistema Octal si antecedes con 0o 0O
+print(0o264)
+#Puedes usar sistema Hexadecimal si antecedes con 0x 0X
+print(0x5A7C)
+#Puedes usar valores flotantes o con punto decimal, importa por como los trata Phyton
+print(0.4)
+#Numeros grandes o pequeños pueden usar notacion cientifica con e E
+print(3e8)
+print(3E-8)
+#Si se requiere usar las "" o ' dentro de la cadena se usa el caracter de escape \
+print('I\'m Monty Python.')
+#Valores Booleanos solo incluyen True o False, se usa para comparar
+print(True > False)
+print(True < False)
+
+#Prueba LAB
+print('"Estoy"\n', '""Aprendiendo""\n', '"""Phyton"""')
