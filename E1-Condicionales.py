f8bde74f9ad681f7b7d647d3bf5e21c9a529ab28
ErickRG2004 <erick.ramirezg04@gmail.com>
2026-02-27 17:26:29 -0600

    Practica 7

---
+++ E1-Condicionales.py
@@ -0,0 +1,40 @@
+#Operadores de comparación
+#Solo devuelven valores True o False
+#== para preguntar si un elemento es igual a otro
+var = 0  # Asignando 0 a var
+print(var == 0)
+
+var = 1  # Asignando 1 a var
+print(var == 0)
+
+#Operador no es igual a  !=
+var = 0  # Asignando 0 a var
+print(var != 0)
+ 
+var = 1  # Asignando 1 a var
+print(var != 0)
+
+#Operadores mayor que > y menor que <
+#Operadores mayor o igual que >= y menor o igual que <=
+
+#Condiciones y ejecución condicional, usar sangria para todas las
+#instrucciones que pertenezcan al if
+
+
+#if sheep_counter >= 120: # Evaluar una expresión condicional
+    #sleep_and_dream()
+
+#Condicional if-else
+2000%4
+
+#Sentencia elif: verifica una condiciones y se detiene cuando encuentra
+#La primera sentencia verdadera
+
+
+#Anidadas
+entrada = input()
+if entrada == "espatifilo": 
+    print("No, ¡quiero un gran Espatifilo!")
+elif entrada == "Espatifilo": 
+    print("Si, ¡El Espatifilo! es la mejor planta de todos los tiempos!")
+else: print("¡Espatifilo!, ¡No ", entrada)
