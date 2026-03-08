99eca18d675253945e10aa01cb720568efd7784d
ErickRG2004 <erick.ramirezg04@gmail.com>
2026-03-07 18:12:25 -0600

    Practica6 E2

---
+++ 27-E2-Caracteres y cadenas.py
@@ -0,0 +1,20 @@
+#Caracteres y cadenas
+#Dado que las computadoras almacenan los caracteres como numeros desarrolamos
+#Un sistema para relizar la conversión
+#El denominado ASCII (por sus siglas en inglés American Standard Code for
+#Information Interchange). El Código Estándar Americano para Intercambio
+#de Información es el más utilizado, y es posible suponer que casi todos
+#los dispositivos modernos (como computadoras, impresoras, teléfonos móviles,
+#tabletas, etc.) usan este código.
+
+#Puntos de codigo:Un punto de código es un numero que compone un caracter.
+#Por ejemplo, el 32 es un punto de código que compone un espacio en
+#codificación ASCII. Podemos decir que el código ASCII estándar consta de
+#128 puntos de código.
+#Como el ASCII estándar ocupa 128 de 256 puntos de código posibles, solo puedes
+#hacer uso de los 128 restantes.
+
+#UNICODE utiliza diferentes formas de codificación cuando se trata de
+#almacenar los caracteres usando archivos o memoria de computadora: dos de
+#ellas son UCS-4 y UTF-8(esta última es la más común ya que desperdicia menos
+#espacio de memoria).
